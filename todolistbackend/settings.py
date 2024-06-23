b7900d0375e707261d3bf5ae488fea3d00bcc270
Lukas Nolting <lukas.nolting@icloud.com>
2024-06-23 14:45:54 +0200

    todos restapi

--- todolistbackend/settings.py
+++ todolistbackend/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure-r&rjv4($u4vv7so-yt7-j4!^wd%x0soma)33xo(kc**9kpf933
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+
+]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+    ]
 
 
 # Application definition
@@ -38,10 +45,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'todolist',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +132,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
